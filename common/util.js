e6be5d71eb0ea70e37958f4605bb1329c869d3f1
chenjiahui <mr_chenjiahui@163.com>
2018-05-12 20:13:58 +0800

    修复了查询中带有分页的参数

--- common/util.js
+++ common/util.js
@@ -27,15 +27,17 @@ let formDataParser = obj => {
 let queryConditionParser = (params) => {
     let condition = {}
     for (let key in params) {
-        if (key.startsWith('like%')) {
-            let field = key.replace('like%', '')
-            condition = (params[key] === undefined || params[key] === '' || params[key] === null) ? {...condition} : {...condition, [field]: {'$like': `%${params[key]}%`}}
-        } else {
-            condition = params[key] === undefined ? {...condition} : {...condition, [key]: params[key]}
+        if (key !== 'limit' && key !== 'currentPage') {
+            if (key.startsWith('like%')) {
+                let field = key.replace('like%', '')
+                condition = (params[key] === undefined || params[key] === '' || params[key] === null) ? {...condition} : {...condition, [field]: {'$like': `%${params[key]}%`}}
+            } else {
+                condition = params[key] === undefined ? {...condition} : {...condition, [key]: params[key]}
+            }
         }
     }
     let opt = { where: condition }
-    if (params.limit !== undefined && params.currentPage !== undefined) {
+    if (params.limit !== undefined && params.currentPage !== undefined && params.limit !== null && params.currentPage !== null) {
         opt.limit = params['limit'],
         opt.offset = (params['currentPage'] - 1) * params['limit']
     }
